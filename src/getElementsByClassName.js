b2a80bcd11cb6de2cd0ea1c0fe2c4f24f4cf9c50
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-07-15 20:57:18 -0700

    finished bare reqs for recursion review

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,38 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // Initialize empty var result = []
+  // return inner function, one parameter
+  // within inner function, check Element.classList if exists, and if it contains that className, push className into result
+  // check if it has childNodes, iterate through all children recursively
+  // outside of the inner function, invoke inner function, return result
+  
+  var result = [];
+  function inner(node) {
+    if (node.classList && node.classList.contains(className)){
+      result.push(node);
+    }
+    if (node.hasChildNodes()){
+      for (var i = 0 ; i < node.childNodes.length ; i++){
+        inner(node.childNodes[i]);
+      }
+    }
+  }
+  
+  inner(document.body);
+  return result; 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 };
